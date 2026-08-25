119f733c129bdcb93d44ff51efaf383e83f75d5a
matty-23 <matu.naser@gmail.com>
2026-08-25 09:46:48 -0300

    Se terminaron los module. Corre bien. Falta testear

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UsuariosModule } from './modules/usuario.module'; // Ajustá la ruta según tus carpetas
+import { UsuariosModule } from './modules/UsuarioModule'; // Ajustá la ruta según tus carpetas
 import { ConfigModule } from '@nestjs/config';
 import { EventoModule } from './modules/EventoModule';
 import { AuthModule } from './modules/AuthModule';
+import {CorreoModule} from "./modules/CorreoModule"
 
 @Module({
-  imports: [UsuariosModule, EventoModule, AuthModule,
+  imports: [UsuariosModule, EventoModule, AuthModule,CorreoModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),],
--- src/services/CorreoService.ts
+++ src/services/CorreoService.ts
@@ -1,9 +1,11 @@
+import { Injectable,Inject } from "@nestjs/common";
 import { CorreoDTO,CorreoConfirmacionCuentaDTO,CorreoRecuperacionContrasenaDTO } from "../DTO/CorreoDTO";
-import { ICorreoClient } from "../interfaces/ICorreoClient";
+import { type ICorreoClient } from "../interfaces/ICorreoClient";
 import { ICorreoService } from "../interfaces/ICorreoService";
 
+@Injectable()
 export class CorreoService implements ICorreoService{
-    constructor(private readonly correoClient:ICorreoClient){}
+    constructor(@Inject('ICorreoClient')private readonly correoClient:ICorreoClient){}
     //En el futuro se puede hacer que con el DTO de confirmacion solicitud evento tambien viaje el id del mismo
     async enviarNotificacion(correo: CorreoDTO, headers: Record<string, any>):Promise<boolean> {
         return await this.correoClient.enviarNotificacion(correo,headers);
